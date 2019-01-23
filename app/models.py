0c7f6b7808e5b7e03874d68be23b88b273a8a95a
mihalw28 <michal.waszak@yahoo.com>
2019-01-23 22:05:29 +0100

    Update app folder.
    Update templates, add 404, 500, _post, edit_profile, user. Update forms, routes, models, config, add errors.

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db, login
 from flask_login import UserMixin
+from hashlib import md5
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,6 +13,9 @@ class User(UserMixin, db.Model):
     club_site_login = db.Column(db.String(64))
     club_site_password = db.Column(db.String(128))
     activities = db.relationship('Activity', backref='person', lazy='dynamic')
+    about_me = db.Column(db.String(140)) 
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow) # change to last visit in a gym :)
+    
 
 
     def __repr__(self):
@@ -22,6 +26,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=retro&s={}'.format(
+            digest, size)
 
 @login.user_loader
 def load_user(id):
