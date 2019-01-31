b4d18e2c995ff7b662a41137959653b52ea0ebbc
mihalw28 <michal.waszak@yahoo.com>
2019-01-31 14:18:53 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 import unittest
 from app import app, db
-from app.models import User
+from app.models import User, Activity
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
@@ -27,6 +27,24 @@ class UserModelCase(unittest.TestCase):
     def test_cell_number(self): # think it is something wrong here
         u = User(username='michal', cell_number='48509590590')
         self.assertNotEqual(u.cell_number, ('48590390590'))
+
+    def test_follow_activities(self):
+        u1 = User(username='sam', email='sam@example.com')
+        u2 = User(username='magi', email='magi@example.com')
+        db.session.add_all([u1, u2])
+
+        #create 2 activities
+        now = datetime.utcnow()
+        a1 = Activity(activ_body='yoga', author=u1, timestamp=now + timedelta(seconds=1))
+        a2 = Activity(activ_body='bodypump', author=u2, timestamp=now + timedelta(seconds=2))
+        db.session.add_all([a1, a2])
+        db.session.commit()
+
+        #check the followed activities
+        f1 = u1.followed_activities().all()
+        f2 = u2.followed_activities().all()
+        self.assertEqual(f1, [a1])
+        self.assertEqual(f2, [a2])
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
