d2b26ad0b85fdc02a4ddea4b6e14781a7df019ee
mihalw28 <michal.waszak@yahoo.com>
2019-05-29 18:38:04 +0200

    Fix '/signup' route after gym site changes.

--- app/workouts/routes.py
+++ app/workouts/routes.py
@@ -26,7 +26,7 @@ def signup():
         users = User.query.all()
         for user in users:
             if (user.club_name is not None) and (user.classes is not None):
-                arg_list = local_options()  # local for now
+                arg_list = local_options(webdriver)  # local for now
                 driver = get_driver(arg_list)
                 log_user(user, driver)
                 today = datetime.date.today()
@@ -36,79 +36,100 @@ def signup():
                 driver.get(list_url)
                 time.sleep(3)
 
-                # List all activities in one day
+                # List all activities and hours from one day
                 list_all_day_act = driver.find_elements_by_css_selector(
                     ".cp-class-list .cp-class-list-day-col .calendar-item-name"
                 )
                 list_all = [element.text.lower() for element in list_all_day_act]
+                lst_all_day_hrs = driver.find_elements_by_css_selector(
+                    ".cp-class-list .cp-class-list-day-col .calendar-item-start"
+                )
+                lst_hrs = [element.text for element in lst_all_day_hrs]
 
-                # List all bookable activities and their start time
+                # Dict from hours and activities
+                dict_all_and_hrs = {}
+                for key, value in zip(lst_hrs, list_all):
+                    if key not in dict_all_and_hrs:
+                        dict_all_and_hrs[key] = []
+                    dict_all_and_hrs[key].append(value)
+                list_all_keys = list(dict_all_and_hrs)  # List all hours (without doubles)
+
+                # Get user training index in all activities
+                user_training = user.classes.lower()
+                for item in dict_all_and_hrs:
+                    for element in dict_all_and_hrs[item]:
+                        if user_training in element:
+                            hour_key = item
+                            hour_activities_lst = dict_all_and_hrs[item]
+                            usr_class_idx = hour_activities_lst.index(element)
+
+                # List all bookable activities and their start time packed into dict
                 list_all_bookable_act = driver.find_elements_by_css_selector(
                     ".cp-class-list .cp-class-list-day-col .is-bookable .calendar-item-name"
                 )
                 list_bookable = [
                     element.text.lower() for element in list_all_bookable_act
                 ]
-
-                # List all activities' start time
                 list_hours = driver.find_elements_by_css_selector(
                     ".cp-class-list .cp-class-list-day-col .is-bookable .calendar-item-start"
                 )
                 list_all_start_time = [element.text for element in list_hours]
 
-                # Combine date & start hour strings, then list parsed
-                date_hour = [(tommorow + " " + hour) for hour in list_all_start_time]
+                dict_all_and_hrs_bookable = {}
+                for key, value in zip(list_all_start_time, list_bookable):
+                    if key not in dict_all_and_hrs_bookable:
+                        dict_all_and_hrs_bookable[key] = []
+                    dict_all_and_hrs_bookable[key].append(value)
+                list_bookable_keys = list(
+                    dict_all_and_hrs_bookable
+                )  # List bookable hours
 
-                # Parse dates
-                parsed_dates = [
-                    dateparser.parse(element, language=["pl"]) for element in date_hour
-                ]
+                # Combine date & start hour strings
+                date_hour = tommorow + " " + str(hour_key)
 
-                user_training = user.classes.lower()
-                if user_training in list_bookable:
-                    # Find index of desirable workout and book
+                for element in list_bookable:
+                    if user_training in element:
+                        web_idx = str(usr_class_idx + 1)
+                        hour_web_idx = str(
+                            list_all_keys.index(hour_key) + 1
+                        )  # From gym website
+                        driver.find_element_by_css_selector(
+                            ".cp-class-list > div:nth-of-type("
+                            + hour_web_idx
+                            + ") > div:nth-of-type("
+                            + web_idx
+                            + ") .class-item-actions"
+                        ).click()
+                        time.sleep(0.5)
 
-                    """
-                    TO DO: change below part -> gym site's been changed recently
-                    and this won't work any longer.
-                    """
-                    workout_index = list_all.index((user.classes).lower())
-                    web_index = str(workout_index + 2)  # From gym website
-                    driver.find_element_by_css_selector(
-                        ".cp-class-container > div:nth-of-type(2) > div:nth-of-type("
-                        + web_index
-                        + ") .class-item-actions"
-                    ).click()
-                    time.sleep(0.5)
-                    """
-                    End of TO DO
-                    """
+                        training_datetime = dateparser.parse(
+                            date_hour
+                        )  # , languages=['pl'])
+                        training_activity = Train(
+                            your_training=user.classes,
+                            training_datetime=training_datetime,
+                            user_id=user.id,
+                        )
+                        db.session.add(training_activity)
+                        db.session.commit()
+                        driver.close()
 
-                    training_datetime = parsed_dates[workout_index - 1]
-                    training_activity = Train(
-                        your_training=user.classes,
-                        training_datetime=training_datetime,
-                        user_id=user.id,
-                    )
-                    db.session.add(training_activity)
-                    db.session.commit()
-                    driver.close()
-                    # Send sms
-                    # Below body_message is one liner and written in
-                    # "bad formatting" due to gsm coding restrictions
-                    body_message = f'Hej, trenujesz {user_training} w dniu {training_datetime}! Zaakceptuj - "tak" lub "nie". Jak nie potwierdzisz uczestnictwa do 4 godzin przed startem treninu, twoja rezerwacja zostanie anulowana.'
-                    client = Client(
-                        current_app.config["TWILIO_ACCOUNT_SID"],
-                        current_app.config["TWILIO_AUTH_TOKEN"],
-                    )
-                    message = client.messages.create(
-                        body=body_message,
-                        from_=current_app.config["TWILIO_PHONE_NUMBER"],
-                        to="+48" + str(user.cell_number),
-                    )
-                    print(message.sid)
-                else:
-                    pass  # sth here in near future
+                        # Send sms
+                        # Below body_message is one liner and written in
+                        # "bad formatting" due to gsm coding restrictions
+                        body_message = f'Hej, trenujesz {user_training} w dniu {training_datetime}! Zaakceptuj - "tak" lub "nie". Jak nie potwierdzisz uczestnictwa do 4 godzin przed startem treninu, twoja rezerwacja zostanie anulowana.'
+                        client = Client(
+                            current_app.config["TWILIO_ACCOUNT_SID"],
+                            current_app.config["TWILIO_AUTH_TOKEN"],
+                        )
+                        message = client.messages.create(
+                            body=body_message,
+                            from_=current_app.config["TWILIO_PHONE_NUMBER"],
+                            to="+48" + str(user.cell_number),
+                        )
+                        print(message.sid)
+                    else:
+                        pass  # sth here in near future
             else:
                 pass  # like above
     return
@@ -135,7 +156,7 @@ def sms():
         db.session.commit()
         resp.message("Dzieki za potwierdzenie. Udanego treningu.")
     elif "nie" in body.lower():
-        arg_list = local_options()
+        arg_list = local_options(webdriver)
         driver = get_driver(arg_list)
         log_user(user, driver)
 
@@ -178,7 +199,7 @@ def cancel_training():
         user = User.query.filter_by(
             username=current_user.username
         ).first()  # Find user by cell_number.
-        arg_list = local_options()
+        arg_list = local_options(webdriver)
         driver = get_driver(arg_list)
         log_user(user, driver)
         date = datetime.date.today()
@@ -221,7 +242,7 @@ def unbook():
             delta_hours = (last_training_time - datetime.datetime.now()).seconds / 3600
             if delta_hours <= 4.0:
                 if is_confirmed == "nie":
-                    arg_list = local_options()
+                    arg_list = local_options(webdriver)
                     driver = get_driver(arg_list)
                     log_user(user, driver)
                     today = datetime.date.today()
