bc218970422ecc0e5b258592166daae6e8d47efb
mihalw28 <michal.waszak@yahoo.com>
2019-07-11 18:58:33 +0200

    Fix loading .env files and delete host

--- fit_app.py
+++ fit_app.py
@@ -7,7 +7,8 @@ app = create_app()
 application = app
 
 if __name__ == "__main__":
-    application.run(host="0.0.0.0")
+    # application.run(host="0.0.0.0")
+    application.run()
 
 
 @app.shell_context_processor
